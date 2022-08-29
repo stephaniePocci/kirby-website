445b8c5885cbc94900a020b658c8537d3d56c1c3
Stephanie Pocci <spokey@csu.fullerton.edu>
2022-08-29 14:56:41 -0700

    Start over lol

--- README.md
+++
@@ -1,2 +0,0 @@
-# kirby-website
-A simple Kirby themed website I'm making to learn more about Svelte, HTML, and CSS!
--- src/lib/components/Text.svelte
+++
@@ -1,5 +0,0 @@
-<div
-  class="container mx-auto text-lg font-normal leading-relaxed md:max-w-prose prose dark:prose-invert"
->
-  <slot />
-</div>
